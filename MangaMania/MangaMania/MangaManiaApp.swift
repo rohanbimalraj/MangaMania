c7d6f4ccc5cafd9e90f159a314e45bebc466b2f0
Rohan Bimal Raj <rohanbimalraj@gmail.com>
2023-07-12 18:01:43 +0530

    First Commit - 12/7/2023

---
+++ MangaMania/MangaMania/MangaManiaApp.swift
@@ -0,0 +1,17 @@
+//
+//  MangaManiaApp.swift
+//  MangaMania
+//
+//  Created by Rohan Bimal Raj on 12/07/23.
+//
+
+import SwiftUI
+
+@main
+struct MangaManiaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
