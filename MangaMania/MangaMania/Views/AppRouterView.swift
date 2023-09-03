2d32d74ec5192aa19093548fe68ad81e7a28f62a
Rohan Bimal Raj <rohanbimalraj@gmail.com>
2023-09-03 15:18:44 +0530

    Logic change for force update

--- MangaMania/MangaMania/Views/AppRouterView.swift
+++ MangaMania/MangaMania/Views/AppRouterView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct AppRouterView: View {
     
+    @Environment(\.scenePhase) private var scenePhase
     @StateObject private var vm = ViewModel()
     
     var body: some View {
@@ -21,11 +22,16 @@ struct AppRouterView: View {
                     .transition(.opacity)
             }
         }
-        .onAppear {
-            DispatchQueue.main.asyncAfter(deadline: .now() + 3.0) {
-                vm.checkForUpdate()
+        .onChange(of: scenePhase, perform: { newPhase in
+            switch newPhase {
+            case .active:
+                DispatchQueue.main.asyncAfter(deadline: .now() + 3.0) {
+                    vm.checkForUpdate()
+                }
+            default:
+                break
             }
-        }
+        })
         .animation(.easeInOut(duration: 0.5), value: vm.showSplash)
         .alert("Update Available", isPresented: $vm.showUpdateAlert) {
             Button {
